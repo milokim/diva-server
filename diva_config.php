e238070fc1b53f1192add63d2788b7e9ac52d2e5
Milo Kim <woogyom.kim@gmail.com>
2017-01-24 13:11:21 +0100

    diva_config: Use domain name instead of IP
    To work properly, each domain name should be defined in /etc/hosts.
    
    Signed-off-by: Milo Kim <woogyom.kim@gmail.com>

--- diva_config.php
+++ diva_config.php
@@ -2,9 +2,9 @@
 
 /* DIVA configuration */
 
-define('DIVA_TARGET_IP', "127.0.0.1");
+define('DIVA_TARGET_IP', "diva-target"); /* diva-target is defined in /etc/hosts */
 define('DIVA_TARGET_PORT', 3505);
-define('DIVA_CAPTURE_IP', "127.0.0.1");
+define('DIVA_CAPTURE_IP', "diva-capture"); /* diva-capture is defined in /etc/hosts */
 define('DIVA_CAPTURE_PORT', 3506);
 
 ?>
